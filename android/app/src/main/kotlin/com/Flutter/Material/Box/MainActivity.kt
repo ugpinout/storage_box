26edb4f0ff99ae0a2b958ebd2c5b16bf8a4fe599
ugpinout <2280711844@qq.com>
2024-09-20 15:10:53 +0800

    Master

--- android/app/src/main/kotlin/com/Flutter/Material/Box/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.Flutter.Material.Box
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/Flutter/Material/flutter_application_1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.Flutter.Material.flutter_application_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
